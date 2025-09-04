bb6205e987c4cf1b21677ea396444296ea976d8f
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2025-09-04 01:09:25 -0300

    adicionando descrição do sistema

--- modulo-3/app.py
+++ modulo-3/app.py
@@ -1,3 +1,7 @@
+#sistema de autenticação integrado a um banco de dados que vai armazenar
+#usuários e a partir dele gerenciamento de perfis que define
+#qual usuário e o que ele pode fazer dentro de cada endpoint da API
+
 from flask import Flask
 
 app = Flask(__name__) #Name para executar de forma manual
