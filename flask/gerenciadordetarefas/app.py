a6254b29fff6cb859cc7db614419b0f800b6e1d0
vitor gomes <vitor.araujo.derreteman4@gmail.com>
2026-03-27 13:41:23 -0300

    terminando rotas crud e fazendo script de teste

--- flask/gerenciadordetarefas/app.py
+++ flask/gerenciadordetarefas/app.py
@@ -28,7 +28,7 @@ def create_task():
 
     tasks_id_control += 1
 
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route("/tasks", methods=['GET'])
 def get_tasks():
